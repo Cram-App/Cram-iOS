45cca7ebf95262fb2130a35f85e51915eb541d90
pablogarces16 <pablogarcesee@gmail.com>
2017-12-02 09:41:50 -0500

    Added login page

---
+++ Cram/Cram/LoginVC.swift
@@ -0,0 +1,42 @@
+//
+//  LoginVC.swift
+//  Cram
+//
+//  Created by Pablo Garces on 12/2/17.
+//  Copyright © 2017 Cram. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var fbBtnBack: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        fbBtnBack.layer.cornerRadius = 6
+        fbBtnBack.layer.masksToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginBtnClicked(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
